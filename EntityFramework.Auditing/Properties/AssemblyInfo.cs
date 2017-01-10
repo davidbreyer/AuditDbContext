fe825fd3e264a31d0bab26c5bf51302a3dadd94e
David Breyer <davidbreyer@gmail.com>
2017-01-10 12:40:07 -0500

    Initial Check-in

---
+++ EntityFramework.Auditing/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Russell Wilkins">
+//     Copyright (c) Russell Wilkins 2012. All Rights Reserved.
+// </copyright>
+// <author>Russell Wilkins</author>
+// <license href="license.txt">
+//     The use of this software is governed by the Microsoft Public License
+//     which is included with this distribution.
+// </license>
+//-----------------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AuditDbContext")]
+[assembly: AssemblyDescription("Entity Framework Auditing")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Russell Wilkins")]
+[assembly: AssemblyProduct("EntityFramework.Auditing")]
+[assembly: AssemblyCopyright("Copyright © Russell Wilkins 2012. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("835c386f-9caf-4bc5-bbb7-46956d2e23a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
